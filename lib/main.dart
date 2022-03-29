a7149eeed8702f258ce7e179f1cbc0a105cda1de
Aquarius-blake <danbaid72@gmail.com>
2022-03-29 16:03:27 +0000

    Minor Update
    Signed-off-by: Aquarius-blake <danbaid72@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,19 @@
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  home: Ninjac()
 ));
 
-int dem=1;
-
-class Home extends StatelessWidget {
+class Ninjac extends StatelessWidget {
 
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-
+    return Scaffold(
       appBar: AppBar(
-        title:Row(
-          children:<Widget>[
-            Icon(Icons.insert_emoticon),
-            Center(
-          child: Padding(padding: EdgeInsets.fromLTRB(90.0, 0, 0, 0) ,
-          child:Text(
-            "Lesson 2",
-            style: TextStyle(
-                color: Colors.black
-            ),
-          ),
-            ),
-        ),
-           Container(
-             child:Padding(padding:EdgeInsets.fromLTRB(84.0, 0, 0, 0),
-             child:  IconButton(
-                 icon: Icon(Icons.add_shopping_cart),
-                 onPressed: (){
-
-                   dem++;
-                   print(dem);
-             }
-             ),
-           )
-           ),
-          ]
-        ),
-
-        backgroundColor: Colors.yellow[300],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children:<Widget>[
-      Row(
-
-        children: <Widget>[
-          Center(
-         child:
-           Container(
-             padding: EdgeInsets.all(10.0),
-              color: Colors.grey[300],
-                child: Text('$dem'),
-            ),
-         ),
-
-          Container(
-            padding: EdgeInsets.fromLTRB(10, 0, 0, 455.0),
-
-              child: Image.asset("Assets/bond.jpg"),
-
-          )
-        ],
-      ),
-        ],
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print("You are An idiot");
-        },
-        child: Text("Help"),
-        backgroundColor: Colors.blue[300],
+        title: Text("Ninja"),
       ),
     );
   }
 }
-
-//  icon button
-// Center(
-/* child: IconButton(
-onPressed: () {
-print("Warning");
-},
-icon: Icon(Icons.access_alarm),
-
-),
-),*/
