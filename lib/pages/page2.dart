7ad81fd5423d1d0b3bf7e3b605eb87c00db13cee
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-09 15:53:31 +0000

    Minor Update
    Signed-off-by: Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -12,30 +12,30 @@ class _p2State extends State<p2> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-appBar: AppBar(
-  backgroundColor: Colors.black,
-  elevation:2.5 ,
-title: Row(
-children:<Widget> [
-  Icon(Icons.wifi),
-  Center(
-    child: Padding(
-      padding: const EdgeInsets.fromLTRB(30.0,0,0,0),
-      child: Text(
-          "Page 2",
-        style: TextStyle(
-fontWeight: FontWeight.bold,
-letterSpacing: 1.5,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation:2.5 ,
+        title: Row(
+          children:<Widget> [
+            Icon(Icons.wifi),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(30.0,0,0,0),
+                child: Text(
+                  "Page 2",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5,
 
 
+                  ),
+                ),
+              ),
+            )
+
+          ],
         ),
       ),
-    ),
-  )
-
-],
-),
-),
       body: Column(
         children: <Widget>[
 
