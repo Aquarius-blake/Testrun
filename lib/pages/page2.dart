b97b05a337361f44d94322a2ddaf8157e2b6a1f4
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-09 16:25:08 +0000

    Minor Update
    Signed-off-by: Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -13,7 +13,7 @@ class _p2State extends State<p2> {
     Dat(name: "Combat Type",info: "Close Combat"),
     Dat(name:"Speciality",info: "Ninjutsu"),
     Dat(name: "Ultimate Technique",info: "Susano\'o: Amatarasu Flame Control"),
-Dat(name: "Rank",info: "Special Jounin")
+    Dat(name: "Rank",info: "Special Jounin")
   ];
 
   @override
